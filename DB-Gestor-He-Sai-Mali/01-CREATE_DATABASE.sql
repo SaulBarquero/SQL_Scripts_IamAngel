38ab9c963c7b035569d9e54f31fa72e071fd3455
SaulBarquero <138588182+SaulBarquero@users.noreply.github.com>
2025-11-09 23:26:16 -0600

    Create 01-CREATE_DATABASE.sql
    Creación de la DB para el proyecto de basededatos

---
+++ DB-Gestor-He-Sai-Mali/01-CREATE_DATABASE.sql
@@ -0,0 +1,8 @@
+-- =========== 1. CREACIÓN DE LA BASE DE DATOS ===========
+-- (Este comando debes ejecutarlo conectado a la BD 'postgres')
+
+CREATE DATABASE gestor_he;
+
+-- Conecta a la nueva base de datos (comando para psql)
+-- (Si usas pgAdmin, ignoramos esta línea y abrimos un Query Tool nuevo en 'gestor_he')
+\c gestor_he
