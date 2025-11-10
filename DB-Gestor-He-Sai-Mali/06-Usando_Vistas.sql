49e1bbc55f7de1cb4fe6f2830e4374a9f0cc3245
SaulBarquero <138588182+SaulBarquero@users.noreply.github.com>
2025-11-09 23:42:13 -0600

    Create 06-Usando_Vistas.sql

---
+++ DB-Gestor-He-Sai-Mali/06-Usando_Vistas.sql
@@ -0,0 +1,8 @@
+-- Para ver todos los detalles de pedidos:
+SELECT * FROM vistapedidosdetallados;
+
+-- Para ver el reporte de gastos por cliente:
+SELECT * FROM vistasumencliente;
+
+-- Para ver el reporte de los meseros total de pedidos atendidos
+SELECT * FROM vistareportemeseros
